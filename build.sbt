993d99e84c300845dc3b620ca32e8e4d324b497f
Jon Hunter <jonhunter1977@gmail.com>
2017-08-03 14:50:12 +0100

    added example kafka stream and parse json

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.1.1",
   "org.apache.spark" %% "spark-streaming" % "2.1.1",
-  "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0"
+  "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0",
+  "org.json4s" % "json4s-native_2.11" % "3.5.3"
 )
--- src/main/scala/twitter-kafka-streaming.scala
+++ src/main/scala/twitter-kafka-streaming.scala
@@ -4,27 +4,52 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.json4s.JsonAST.{JString, JValue}
+import org.json4s.jackson.JsonMethods._
 
-object TwitterKafkaStreaming extends App {
-  val conf = new SparkConf().setAppName("twitter-kafka-streaming").setMaster("local[*]")
-  val ssc = new StreamingContext(conf, Seconds(5))
+object TwitterKafkaStreaming {
 
-  val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "192.168.10.4:9092,192.168.10.4:9093,192.168.10.4:9094",
-    "key.deserializer" -> classOf[StringDeserializer],
-    "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "SparkStreamingConsumer",
-    "auto.offset.reset" -> "earliest",
-    "enable.auto.commit" -> (false: java.lang.Boolean)
-  )
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("twitter-kafka-streaming").setMaster("local[*]")
+    val ssc = new StreamingContext(conf, Seconds(5))
 
-  val topics = Array("twitter-stream")
-  val messages = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "192.168.10.4:9092,192.168.10.4:9093,192.168.10.4:9094",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "SparkStreamingConsumer",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
 
-  val tweets = messages.map(message => (message.key, message.value))
+    val topics = Array("twitter-stream")
+    val messages = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
 
-  tweets.print()
+    val tweets = messages
+      .map(message => parseMessageJson(message.value))
+      .filter(message => filterToLanguage(message, "en"))
+      .map(message => getTweet(message))
 
-  ssc.start()
-  ssc.awaitTermination()
+    tweets.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def parseMessageJson(message: String): JValue = {
+    val json = raw"""$message"""
+    parse(json, useBigDecimalForDouble = true)
+  }
+
+  def filterToLanguage(message: JValue, language: String): Boolean = {
+    val lang = (message \ "lang").values
+    if(lang == language) true else false
+  }
+
+  def getTweet(message: JValue): String = {
+    (message \ "text").values.toString
+  }
 }
+
+
+
